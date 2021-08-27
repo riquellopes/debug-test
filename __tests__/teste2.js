5b4d21225331c77790f3dd71488fb021024669c1
Henrique Lopes <riquellopes@gmail.com>
2021-08-27 12:54:25 -0300

    removed test

--- __tests__/teste2.js
+++ __tests__/teste2.js
@@ -4,8 +4,4 @@ describe("Validate debug", () => {
     test("Should 1+1 = 2", () => {
         expect( 1 + 1).toEqual(2)
     })
-
-    test("Should 1+2 = 3", () => {
-        expect( 1 + 2).toEqual(3)
-    })
 })
