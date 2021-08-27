d536cd774353a798837aa07f2c1f2c9a11aeab5f
Henrique Lopes <riquellopes@gmail.com>
2021-08-27 12:50:28 -0300

    created a new test

--- __tests__/teste2.js
+++ __tests__/teste2.js
@@ -1,7 +1,11 @@
 describe("Validate debug", () => {
 
 
-    test("Should 1+1 it is 2", () => {
+    test("Should 1+1 = 2", () => {
         expect( 1 + 1).toEqual(2)
+    })
+
+    test("Should 1+2 = 3", () => {
+        expect( 1 + 2).toEqual(3)
     })
 })
